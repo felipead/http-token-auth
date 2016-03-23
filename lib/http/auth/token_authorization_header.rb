a973ca7759905fdae489f19da92e6934aedf9c17
Felipe Dornelas <felipead@gmail.com>
2016-03-23 18:54:53 -0300

    Parses the Token Authorization Header according to RFC

--- lib/http/auth/token_authorization_header.rb
+++ lib/http/auth/token_authorization_header.rb
@@ -1,13 +1,56 @@
 module HTTP
   module Auth
     class TokenAuthorizationHeader
+      attr_reader :token, :coverage, :nonce, :auth, :timestamp
+
+      def initialize(attributes)
+        remove_trailling_whitespace(attributes)
+        @token = must_have_token(attributes[:token])
+        @coverage = must_have_coverage_or_default(attributes[:coverage])
+        @nonce = attributes[:nonce]
+        @auth = attributes[:auth]
+        @timestamp = attributes[:timestamp]
+      end
+
       def schema
         :token
       end
 
-      def self.parse(attributes)
-        TokenAuthorizationHeader.new
+      def self.parse(attributes_string)
+        groups = extract_groups(attributes_string)
+        TokenAuthorizationHeader.new extract_attributes(groups)
       end
+
+      private
+
+      def must_have_token(token)
+        raise ArgumentError, 'Token attribute is required' unless token && !token.empty?
+        token
+      end
+
+      def must_have_coverage_or_default(coverage)
+        (coverage && !coverage.empty?)? coverage : 'base'
+      end
+
+      def remove_trailling_whitespace(attributes)
+        attributes.each do |name, value|
+          value.strip! unless value.nil?
+        end
+      end
+
+      def self.extract_groups(attributes_string)
+        attributes_string.scan(/(\w+)="([^"]*)"/)
+      end
+
+      def self.extract_attributes(groups)
+        attributes = {}
+        groups.each do |group|
+          attributes[group[0].to_sym] = group[1]
+        end
+        attributes
+      end
+
+      private_class_method :extract_attributes, :extract_groups
     end
   end
 end
--- spec/http/auth/token_authorization_header_spec.rb
+++ spec/http/auth/token_authorization_header_spec.rb
@@ -1,16 +1,71 @@
 require 'http/auth'
 
 describe HTTP::Auth::TokenAuthorizationHeader do
-  let :attributes do
-    <<~EOS
-      token="5ccc069c403ebaf9f0171e9517f40e41"
-    EOS
+  describe 'given a complete token authorization header' do
+    let :complete_header_string do
+      <<-EOS
+        token="5ccc069c403ebaf9f0171e9517f40e41",
+        coverage="base",
+        nonce="2e0d73708933eff3e53319d884a0505c",
+        auth="ed5a137d3724ec12ddd95bbea3e8a634",
+        timestamp="1234567890"
+      EOS
+    end
+
+    let :complete_header do
+      HTTP::Auth::TokenAuthorizationHeader.parse(complete_header_string)
+    end
+
+    it 'returns token as the schema' do
+      expect(complete_header.schema).to eq(:token)
+    end
+
+    it 'returns the "token" attribute' do
+      expect(complete_header.token).to eq('5ccc069c403ebaf9f0171e9517f40e41')
+    end
+
+    it 'returns the "coverage" attribute' do
+      expect(complete_header.coverage).to eq('base')
+    end
+
+    it 'returns the "nonce" attribute' do
+      expect(complete_header.nonce).to eq('2e0d73708933eff3e53319d884a0505c')
+    end
+
+    it 'returns the "auth" attribute' do
+      expect(complete_header.auth).to eq('ed5a137d3724ec12ddd95bbea3e8a634')
+    end
+
+    it 'returns the timestamp attribute' do
+      expect(complete_header.timestamp).to eq('1234567890')
+    end
   end
 
-  describe '#schema' do
-    it 'returns token' do
-      header = HTTP::Auth::TokenAuthorizationHeader.parse(attributes)
-      expect(header.schema).to eq(:token)
+  describe 'given an incomplete token authorization header' do
+    it 'fails if the "token" attribute is ommited' do
+      without_token = 'coverage="base", nonce="2e0d73708933eff3e53319d884a0505c"'
+      expect do
+        HTTP::Auth::TokenAuthorizationHeader.parse without_token
+      end.to raise_error(ArgumentError)
+    end
+
+    it 'fails if the "token" attribute is empty' do
+      with_empty_token = 'token="", coverage="base", nonce="2e0d73708933eff3e53319d884a0505c"'
+      expect do
+        HTTP::Auth::TokenAuthorizationHeader.parse with_empty_token
+      end.to raise_error(ArgumentError)
+    end
+
+    it 'returns "base" if the "coverage" attribute is ommited' do
+      without_coverage = 'token="123123342"'
+      header = HTTP::Auth::TokenAuthorizationHeader.parse without_coverage
+      expect(header.coverage).to eq('base')
+    end
+
+    it 'returns "base" if the "coverage" attribute is empty' do
+      with_empty_coverage = 'token="123142", coverage=""'
+      header = HTTP::Auth::TokenAuthorizationHeader.parse with_empty_coverage
+      expect(header.coverage).to eq('base')
     end
   end
 end
