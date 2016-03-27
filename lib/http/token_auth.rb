da90d7cf37f19334211e9741ebdc5610a312cefb
Felipe Dornelas <felipead@gmail.com>
2016-03-26 23:27:20 -0300

    Token parsing and building now conforms to the specification

--- lib/http/token_auth.rb
+++ lib/http/token_auth.rb
@@ -1,50 +1,83 @@
 module HTTP
+  class TokenAuthArgumentError < StandardError
+    def initialize(argument)
+      super(%(Invalid token credentials: "#{argument}" is missing))
+    end
+  end
+
+  class TokenAuthHeaderParsingError < StandardError
+    def initialize(submessage)
+      super(%(Error parsing token schema "Authorization" header: #{submessage}))
+    end
+  end
+
   class TokenAuth
     attr_reader :token, :coverage, :nonce, :auth, :timestamp
 
-    def initialize(attributes)
-      @token = must_have_token(strip_whitespace(attributes[:token]))
-      @coverage = must_have_coverage_or_default(strip_whitespace(attributes[:coverage]))
-      @nonce = strip_whitespace(attributes[:nonce])
-      @auth = strip_whitespace(attributes[:auth])
-      @timestamp = strip_whitespace(attributes[:timestamp])
+    def initialize(token:, coverage: nil, nonce: nil, auth: nil, timestamp: nil)
+      @token = token
+      @coverage = coverage
+      @nonce = nonce
+      @auth = auth
+      @timestamp = timestamp
+      validate_itself
     end
 
     def self.parse_header(string)
       schema, attributes_string = split_header(string)
       raise ArgumentError, "Invalid schema #{schema}" unless schema == 'Token'
-      TokenAuth.new parse_attributes(attributes_string)
+      from_attributes parse_attributes(attributes_string)
     end
 
-    def to_s
+    def to_header
       attributes = []
       attributes << %(token="#{@token}")
-      attributes << %(coverage="#{@coverage}")
-      attributes << %(nonce="#{@nonce}") if @nonce
-      attributes << %(auth="#{@auth}") if @auth
-      attributes << %(timestamp="#{@timestamp}") if @timestamp
-
+      unless coverage.nil?
+        attributes << %(coverage="#{coverage_name}")
+        attributes << %(nonce="#{@nonce}")
+        attributes << %(auth="#{@auth}")
+        attributes << %(timestamp="#{@timestamp}")
+      end
       "Token #{attributes.join(', ')}"
     end
 
     private
 
-    def must_have_token(token)
-      raise ArgumentError, '"token" attribute is required' unless token && !token.empty?
-      token
+    def validate_itself
+      must_have_token
+      return if @coverage.nil?
+      must_have_nonce
+      must_have_auth
+      must_have_timestamp
     end
 
-    def must_have_coverage_or_default(coverage)
-      (coverage && !coverage.empty?) ? coverage : 'base'
+    def must_have_token
+      raise TokenAuthArgumentError, 'token' if @token.nil? || @token.empty?
     end
 
-    def strip_whitespace(string)
-      string.nil? ? nil : string.strip
+    def must_have_nonce
+      raise TokenAuthArgumentError, 'nonce' if @nonce.nil? || @nonce.empty?
+    end
+
+    def must_have_auth
+      raise TokenAuthArgumentError, 'auth' if @auth.nil? || @auth.empty?
+    end
+
+    def must_have_timestamp
+      raise TokenAuthArgumentError, 'timestamp' if @timestamp.nil?
+    end
+
+    def coverage_name
+      case @coverage
+      when :base then 'base'
+      when :base_body_sha_256 then 'base+body-sha-256'
+      end
     end
 
     def self.split_header(string)
       string.split(' ', 2)
     end
+    private_class_method :split_header
 
     def self.parse_attributes(string)
       attributes = {}
@@ -53,7 +86,31 @@ module HTTP
       end
       attributes
     end
+    private_class_method :parse_attributes
 
-    private_class_method :split_header, :parse_attributes
+    def self.from_attributes(attributes)
+      TokenAuth.new token: attributes[:token],
+                    coverage: parse_coverage(attributes[:coverage]),
+                    nonce: attributes[:nonce],
+                    auth: attributes[:auth],
+                    timestamp: parse_timestamp(attributes[:timestamp])
+    end
+    private_class_method :from_attributes
+
+    def self.parse_coverage(coverage)
+      return nil if coverage.nil? || coverage.empty?
+      case coverage
+      when 'none' then nil
+      when 'base' then :base
+      when 'base+body-sha-256' then :base_body_sha_256
+      else raise TokenAuthHeaderParsingError, %(Invalid coverage "#{coverage}")
+      end
+    end
+    private_class_method :parse_coverage
+
+    def self.parse_timestamp(timestamp)
+      timestamp.nil? ? nil : timestamp.to_i
+    end
+    private_class_method :parse_timestamp
   end
 end
--- spec/http/token_auth_spec.rb
+++ spec/http/token_auth_spec.rb
@@ -1,155 +1,155 @@
 require 'http/token_auth'
 
 describe HTTP::TokenAuth do
-  describe 'given a complete token authorization header' do
-    let :complete_header_with_extra_whitespaces do
-      <<-EOS
-        Token token="5ccc069c403ebaf9f0171e9517f40e41",
-              coverage="foobar",
-              nonce="2e0d73708933eff3e53319d884a0505c",
-              auth="ed5a137d3724ec12ddd95bbea3e8a634",
-              timestamp="1458771255"
-      EOS
+  describe 'given the value of an "Authentication" HTTP header with the token scheme' do
+    describe 'using a cryptographic algorithm' do
+      { 'base' => :base, 'base+body-sha-256' => :base_body_sha_256 }.each do |name, symbol|
+        it %(parses it if coverage is "#{name}") do
+          header = <<-EOS
+            Token token="h480djs93hd8",
+                  coverage="#{name}",
+                  nonce="dj83hs9s",
+                  auth="djosJKDKJSD8743243/jdk33klY=",
+                  timestamp="137131200"
+          EOS
+          credentials = HTTP::TokenAuth.parse_header(header)
+          expect(credentials.token).to eq('h480djs93hd8')
+          expect(credentials.coverage).to eq(symbol)
+          expect(credentials.nonce).to eq('dj83hs9s')
+          expect(credentials.auth).to eq('djosJKDKJSD8743243/jdk33klY=')
+          expect(credentials.timestamp).to eq(137131200)
+        end
+      end
+
+      it 'fails if coverage is not recognized' do
+        header = <<-EOS
+          Token token="h480djs93hd8",
+                coverage="invalid",
+                nonce="dj83hs9s",
+                auth="djosJKDKJSD8743243/jdk33klY=",
+                timestamp="137131200"
+        EOS
+        expect do
+          HTTP::TokenAuth.parse_header(header)
+        end.to raise_error(HTTP::TokenAuthHeaderParsingError).with_message(
+          /Invalid coverage "invalid"/)
+      end
     end
 
-    let :complete_header do
-      complete_header_with_extra_whitespaces.gsub(/\s+/, ' ').strip
-    end
+    describe 'without using a cryptographic algorithm' do
+      it 'parses it if coverage is "none"' do
+        header = <<-EOS
+          Token token="h480djs93hd8",
+                coverage="none"
+        EOS
+        credentials = HTTP::TokenAuth.parse_header(header)
+        expect(credentials.token).to eq('h480djs93hd8')
+        expect(credentials.coverage).to be_nil
+        expect(credentials.nonce).to be_nil
+        expect(credentials.auth).to be_nil
+        expect(credentials.timestamp).to be_nil
+      end
 
-    it 'successfully parses the header string' do
-      parsed = HTTP::TokenAuth.parse_header(complete_header)
-      expect(parsed.token).to eq('5ccc069c403ebaf9f0171e9517f40e41')
-      expect(parsed.coverage).to eq('foobar')
-      expect(parsed.nonce).to eq('2e0d73708933eff3e53319d884a0505c')
-      expect(parsed.auth).to eq('ed5a137d3724ec12ddd95bbea3e8a634')
-      expect(parsed.timestamp).to eq('1458771255')
-    end
-
-    it 'successfully parses the header string regardless of extra whitespace' do
-      parsed = HTTP::TokenAuth.parse_header(complete_header_with_extra_whitespaces)
-      expect(parsed.token).to eq('5ccc069c403ebaf9f0171e9517f40e41')
-      expect(parsed.coverage).to eq('foobar')
-      expect(parsed.nonce).to eq('2e0d73708933eff3e53319d884a0505c')
-      expect(parsed.auth).to eq('ed5a137d3724ec12ddd95bbea3e8a634')
-      expect(parsed.timestamp).to eq('1458771255')
-    end
-
-    it 'builds the header string' do
-      parsed = HTTP::TokenAuth.parse_header(complete_header)
-      expect(parsed.to_s).to eq(complete_header)
+      it 'parses it if coverage is not defined' do
+        header = <<-EOS
+          Token token="h480djs93hd8"
+        EOS
+        credentials = HTTP::TokenAuth.parse_header(header)
+        expect(credentials.token).to eq('h480djs93hd8')
+        expect(credentials.coverage).to be_nil
+        expect(credentials.nonce).to be_nil
+        expect(credentials.auth).to be_nil
+        expect(credentials.timestamp).to be_nil
+      end
     end
   end
 
-  describe 'given an incomplete token authorization header' do
-    describe 'parsing the header string' do
-      it 'fails if "token" is ommited' do
-        without_token = 'Token coverage="base", nonce="2e0d73708933eff3e53319d884a0505c"'
+  describe 'building an "Authentication" HTTP header with the token scheme' do
+    it 'fails if token is not defined' do
+      expect do
+        HTTP::TokenAuth.new token: nil,
+                            coverage: :base,
+                            nonce: 'dj83hs9s',
+                            auth: 'djosJKDKJSD8743243/jdk33klY=',
+                            timestamp: 137131200
+      end.to raise_error(HTTP::TokenAuthArgumentError).with_message(
+        'Invalid token credentials: "token" is missing')
+    end
+
+    describe 'using a cryptographic algorithm' do
+      { 'base' => :base, 'base+body-sha-256' => :base_body_sha_256 }.each do |name, symbol|
+        it %(builds it if coverage is "#{name}") do
+          credentials = HTTP::TokenAuth.new token: 'h480djs93hd8',
+                                            coverage: symbol,
+                                            nonce: 'dj83hs9s',
+                                            auth: 'djosJKDKJSD8743243/jdk33klY=',
+                                            timestamp: 137131200
+
+          header = credentials.to_header
+          expect(header).to start_with('Token')
+          expect(header).to include('token="h480djs93hd8"')
+          expect(header).to include(%(coverage="#{name}"))
+          expect(header).to include('nonce="dj83hs9s"')
+          expect(header).to include('auth="djosJKDKJSD8743243/jdk33klY="')
+          expect(header).to include('timestamp="137131200"')
+        end
+      end
+
+      it 'fails if nonce is not defined' do
         expect do
-          HTTP::TokenAuth.parse_header without_token
-        end.to raise_error(ArgumentError).with_message('"token" attribute is required')
+          HTTP::TokenAuth.new token: 'h480djs93hd8',
+                              coverage: :base,
+                              auth: 'djosJKDKJSD8743243/jdk33klY=',
+                              timestamp: 137131200
+        end.to raise_error(HTTP::TokenAuthArgumentError).with_message(
+          'Invalid token credentials: "nonce" is missing')
       end
 
-      it 'fails if "token" is empty' do
-        with_empty_token = 'Token token="", coverage="base", nonce="2e0d73708933eff3e53319d884a05c"'
+      it 'fails if auth is not defined' do
         expect do
-          HTTP::TokenAuth.parse_header with_empty_token
-        end.to raise_error(ArgumentError).with_message('"token" attribute is required')
+          HTTP::TokenAuth.new token: 'h480djs93hd8',
+                              coverage: :base,
+                              nonce: 'dj83hs9s',
+                              timestamp: 137131200
+        end.to raise_error(HTTP::TokenAuthArgumentError).with_message(
+          'Invalid token credentials: "auth" is missing')
       end
 
-      it 'returns "base" if "coverage" is ommited' do
-        without_coverage = 'Token token="sample_token"'
-        header = HTTP::TokenAuth.parse_header without_coverage
-        expect(header.coverage).to eq('base')
-      end
-
-      it 'returns "base" if "coverage" is empty' do
-        with_empty_coverage = 'Token token="sample_token", coverage=""'
-        header = HTTP::TokenAuth.parse_header with_empty_coverage
-        expect(header.coverage).to eq('base')
-      end
-
-      it 'returns nil if "nonce" is ommited' do
-        without_nonce = 'Token token="sample_token"'
-        header = HTTP::TokenAuth.parse_header without_nonce
-        expect(header.nonce).to be_nil
-      end
-
-      it 'returns nil if "auth" is ommited' do
-        without_auth = 'Token token="sample_token"'
-        header = HTTP::TokenAuth.parse_header without_auth
-        expect(header.auth).to be_nil
-      end
-
-      it 'returns nil if "timestamp" is ommited' do
-        without_timestamp = 'Token token="sample_token"'
-        header = HTTP::TokenAuth.parse_header without_timestamp
-        expect(header.timestamp).to be_nil
+      it 'fails if timestamp is not defined' do
+        expect do
+          HTTP::TokenAuth.new token: 'h480djs93hd8',
+                              coverage: :base,
+                              nonce: 'dj83hs9s',
+                              auth: 'djosJKDKJSD8743243/jdk33klY='
+        end.to raise_error(HTTP::TokenAuthArgumentError).with_message(
+          'Invalid token credentials: "timestamp" is missing')
       end
     end
 
-    describe 'building the header string' do
-      it 'builds with default "coverage"' do
-        header = HTTP::TokenAuth.new(
-          token: 'sample_token',
-          nonce: 'sample_nonce',
-          auth: 'sample_auth',
-          timestamp: '123456789'
-        )
-        header_string = header.to_s
-        expect(header_string).to include('Token')
-        expect(header_string).to include('coverage="base"')
-        expect(header_string).to include('token=')
-        expect(header_string).to include('nonce=')
-        expect(header_string).to include('auth=')
-        expect(header_string).to include('timestamp=')
+    describe 'without using a cryptographic algorithm' do
+      it 'builds it if coverage is "none"' do
+        credentials = HTTP::TokenAuth.new token: 'h480djs93hd8',
+                                          coverage: nil
+
+        header = credentials.to_header
+        expect(header).to start_with('Token')
+        expect(header).to include('token="h480djs93hd8"')
+        expect(header).to_not include('coverage')
+        expect(header).to_not include('nonce')
+        expect(header).to_not include('auth')
+        expect(header).to_not include('timestamp')
       end
 
-      it 'builds without "nonce"' do
-        header = HTTP::TokenAuth.new(
-          token: 'sample_token',
-          coverage: 'sample_coverage',
-          auth: 'sample_auth',
-          timestamp: '123456789'
-        )
-        header_string = header.to_s
-        expect(header_string).to include('Token')
-        expect(header_string).to_not include('nonce=')
-        expect(header_string).to include('token=')
-        expect(header_string).to include('coverage=')
-        expect(header_string).to include('auth=')
-        expect(header_string).to include('timestamp=')
-      end
+      it 'builds it if coverage is not defined' do
+        credentials = HTTP::TokenAuth.new token: 'h480djs93hd8'
 
-      it 'builds without "auth"' do
-        header = HTTP::TokenAuth.new(
-          token: 'sample_token',
-          coverage: 'sample_coverage',
-          nonce: 'sample_nonce',
-          timestamp: '123456789'
-        )
-        header_string = header.to_s
-        expect(header_string).to include('Token')
-        expect(header_string).to_not include('auth=')
-        expect(header_string).to include('token=')
-        expect(header_string).to include('coverage=')
-        expect(header_string).to include('nonce=')
-        expect(header_string).to include('timestamp=')
-      end
-
-      it 'builds without "timestamp"' do
-        header = HTTP::TokenAuth.new(
-          token: 'sample_token',
-          coverage: 'sample_coverage',
-          nonce: 'sample_nonce',
-          auth: 'sample_auth'
-        )
-        header_string = header.to_s
-        expect(header_string).to include('Token')
-        expect(header_string).to_not include('timestamp=')
-        expect(header_string).to include('token=')
-        expect(header_string).to include('coverage=')
-        expect(header_string).to include('nonce=')
-        expect(header_string).to include('auth=')
+        header = credentials.to_header
+        expect(header).to start_with('Token')
+        expect(header).to include('token="h480djs93hd8"')
+        expect(header).to_not include('coverage')
+        expect(header).to_not include('nonce')
+        expect(header).to_not include('auth')
+        expect(header).to_not include('timestamp')
       end
     end
   end
